af312f1d099fdc8790ec0310e45d8f3071eac895
SWAR777 <swardave87@outlook.com>
2025-11-12 22:19:32 +0530

    Prime number checker

---
+++ primenumber.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("Enter a number here:");
+    scanf("%d",&a);
+    if(a<0){
+        return 0;
+    }
+    else if (a>0){
+        if(a==1)
+            printf("Not a Prime number");
+        else{
+            int n,b=1;
+            for(n=2;n<a;n++){
+                if(a%n==0){   
+                    b=0;
+                    break;
+                }
+            }
+            if(b)
+                printf("It is prime number");    
+            else
+                printf("Not a prime number");
+         }    
+    }
+    return 0;
+}
